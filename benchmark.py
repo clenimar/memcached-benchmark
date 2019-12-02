a1273459a46d7c2ec1f1fb16c06ad475b8323b66
Lucas Cavalcante <lucasmcavalcante@gmail.com>
2019-12-02 17:06:07 -0300

    client

--- benchmark.py
+++ benchmark.py
@@ -7,16 +7,15 @@ import pylibmc
 import memcache
 
 
-def run(client, target=1000000):
+def run(pool, target=1000000):
     """run benchmark against a memcached server."""
     # start timer
-    pool = pylibmc.ClientPool(mc, 10)
     starttime = time.time()
     # request count
     r = 0
 
     while r < target:
-        with pool.reserve() as mc:
+        with pool.reserve() as client:
         # do request
 
 
@@ -42,10 +41,11 @@ if __name__ == "__main__":
     print(addresses)
     # start client and load memcached w/ 200 keys
     mc = pylibmc.Client(addresses, binary=True,behaviors={"tcp_nodelay": True,"ketama": True})
+    pool = pylibmc.ClientPool(mc, 10)
     for n in range(200):
         mc.set('bench_key_%d' % n, 'bench_value_%d' % n)
 
     # run and print results
-    result = run(mc, target=args.target)
+    result = run(pool, target=args.target)
     print("exp. time: %f seconds\navg. rps: %f" % (result[1], result[0]))
 
